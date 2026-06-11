bb286e61c2049e87c8f8854102e41023a61ea6b7
Alex Pappas <a.pappas@arcanys.com>
2026-06-12 03:28:09 +0800

    chore(example): disable prefer-standalone lint — intentional, legacy NgModule app

--- projects/ngx-fluent-example/src/app/app.component.ts
+++ projects/ngx-fluent-example/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { NgxFluentService } from '@zeferinix/ngx-fluent';
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
+    // eslint-disable-next-line @angular-eslint/prefer-standalone
     standalone: false
 })
 export class AppComponent implements OnInit {
