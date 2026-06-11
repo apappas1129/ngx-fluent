bedca91fe17c43e9d0c14df5ab150d010670cd01
Alex Pappas <a.pappas@arcanys.com>
2026-06-11 15:53:36 +0800

    refactor(pipe): standalone, DestroyRef, zoneless-safe markForCheck

--- projects/ngx-fluent/src/lib/ngx-fluent.module.ts
+++ projects/ngx-fluent/src/lib/ngx-fluent.module.ts
@@ -3,9 +3,8 @@ import { NgxFluentPipe } from './ngx-fluent.pipe';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 @NgModule({
-  declarations: [NgxFluentPipe],
+  imports: [NgxFluentPipe],
   exports: [NgxFluentPipe],
-  imports: [],
   providers: [provideHttpClient(withInterceptorsFromDi())],
 })
 export class NgxFluentModule {}
--- projects/ngx-fluent/src/lib/ngx-fluent.pipe.ts
+++ projects/ngx-fluent/src/lib/ngx-fluent.pipe.ts
@@ -1,30 +1,31 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { Pipe, PipeTransform } from '@angular/core';
-import { Observable } from 'rxjs';
+import { ChangeDetectorRef, DestroyRef, Pipe, PipeTransform, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { NgxFluentService } from './ngx-fluent.service';
 import { isEqual } from 'lodash-es';
 
 @Pipe({
-    name: 'fluent',
-    pure: false,
-    standalone: false
+  name: 'fluent',
+  pure: false,
+  standalone: true,
 })
 export class NgxFluentPipe implements PipeTransform {
-  value: string | null = null;
+  private readonly fluentService = inject(NgxFluentService);
+  private readonly cdr = inject(ChangeDetectorRef);
+  private readonly destroyRef = inject(DestroyRef);
 
-  localeChanges: Observable<string | null> | null = null;
-
-  previousArgs?: any;
-
-  constructor(private readonly fluentService: NgxFluentService) {}
+  private value: string | null = null;
+  private previousArgs?: any;
+  private initialized = false;
 
   transform(key: string, args?: any) {
-    if (!this.localeChanges) {
-      this.localeChanges = this.fluentService.localeChanges;
+    if (!this.initialized) {
+      this.initialized = true;
 
-      this.localeChanges.subscribe(async () => {
+      this.fluentService.localeChanges.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(async () => {
         this.value = await this.fluentService.translate(key, this.previousArgs ?? args);
+        this.cdr.markForCheck();
       });
     }
 
@@ -33,6 +34,7 @@ export class NgxFluentPipe implements PipeTransform {
       this.previousArgs = args;
       this.fluentService.translate(key, args).then((value) => {
         this.value = value;
+        this.cdr.markForCheck();
       });
     }
 
